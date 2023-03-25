b9396b4c2dbee7bafb64224b8d6546ec300ae614
Dorcy Jean Duke <dorcyd89@gmail.com>
2023-03-24 21:22:40 -0400

    feat: implement docker and create auth with session

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthenticatedGuard } from './auth/authenticated.guard';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @UseGuards(LocalAuthGuard)
+  @Get('/login')
+  login(@Request() req): any {
+    return { msg: 'Logged In!' };
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @Get('protected')
   getHello(): string {
     return this.appService.getHello();
   }
